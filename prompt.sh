9453708a8e37e5c2e960f43aef657132e402d4c7
Morley Zhi <morley@okcupid.com>
2016-10-28 13:33:48 -0400

    get ridda the host

--- prompt.sh
+++ prompt.sh
@@ -11,4 +11,4 @@ function parse_git_branch {
         git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1/"
         #git rev-parse --abbrev-ref HEAD
 }
-export PS1='\[\033[1;32m\]$(date +%-I:%M%p)\[\033[0m\] \[\033[0;37m\]\H\[\033[0m\]:\[\033[0;37m\]\u\[\033[0m\] \w \[\033[0;36m\]$(parse_git_branch)\[\033[0m\] \n\[\033[1;37m\]$\[\033[0m\] '
+export PS1='\[\033[1;32m\]$(date +%-I:%M%p)\[\033[0m\] \w \[\033[0;36m\]$(parse_git_branch)\[\033[0m\] \n\[\033[1;37m\]$\[\033[0m\] '
