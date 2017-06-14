b980ee1f1cdfb48159a26d510c5374e1fbf0866f
Morley Zhi <morley@okcupid.com>
2017-06-14 16:43:16 -0400

    adjust defaults

--- bootstrap.sh
+++ bootstrap.sh
@@ -69,8 +69,8 @@ sudo scutil --set LocalHostName "homer"
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "homer"
 
 # Set fast key repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 
 # Require password as soon as screensaver or sleep mode starts
