1373e1afcf8733c626e22ac90a736e86b769d193
Morley Zhi <morley@stellar.org>
2020-01-23 14:53:38 -0500

    new bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,276 +1,227 @@
 #!/usr/bin/env bash
+#
+# Bootstrap script for setting up a new OSX machine
+#
+# This should be idempotent so it can be run multiple times.
+#
+# Some apps don't have a cask and so still need to be installed by hand. These
+# include:
+#
+# - Twitter (app store)
+# - Postgres.app (http://postgresapp.com/)
+#
+# Notes:
+#
+# - If installing full Xcode, it's better to install that first from the app
+#   store before running the bootstrap script. Otherwise, Homebrew can't access
+#   the Xcode libraries as the agreement hasn't been accepted yet.
+#
+# Reading:
+#
+# - http://lapwinglabs.com/blog/hacker-guide-to-setting-up-your-mac
+# - https://gist.github.com/MatthewMueller/e22d9840f9ea2fee4716
+# - https://news.ycombinator.com/item?id=8402079
+# - http://notes.jerzygangi.com/the-best-pgp-tutorial-for-mac-os-x-ever/
 
-# check that xtools are installed before proceeding
-gcc --help > /dev/null 2>&1 || { echo >&2 "Install XCode and Command Line Tools first!"; exit 1; }
+# helpers
+function echo_ok() { echo -e '\033[1;32m'"$1"'\033[0m'; }
+function echo_warn() { echo -e '\033[1;33m'"$1"'\033[0m'; }
+function echo_error() { echo -e '\033[1;31mERROR: '"$1"'\033[0m'; }
 
+echo_ok "Install starting. You may be asked for your password (for sudo)."
 
-# ===============================================
-# Edit brew and cask packages here!
-# -----------------------------------------------
+# requires xcode and tools!
+xcode-select -p || exit "XCode must be installed! (use the app store)"
 
-PACKAGES=(
-    ack
-    git
-    hub
-    vim
-    wget
-)
-
-CASKS=(
-    adobe-creative-cloud
-    atom
-    caffeine
-    dropbox
-    firefox
-    google-chrome
-    iterm2
-    sketch
-    slack
-    sublime-text3
-    virtualbox
-    vlc
-
-    # morley shit
-    1password
-    amazon-music
-    flux
-    jaikoz
-    sizeup
-    spotify
-    swinsian
-)
-
-
-
-echo "Starting bootstrapping"
-
-
-# ===============================================
-# Edit OS X settings here!
-# -----------------------------------------------
-
-echo "Configuring OSX..."
-
-# Close any open System Preferences panes, to prevent them from overriding
-# settings we’re about to change
-osascript -e 'tell application "System Preferences" to quit'
-
-# Ask for the administrator password upfront
-sudo -v
-
-# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
-
-# Set computer name (as done via System Preferences → Sharing)
-sudo scutil --set ComputerName "homer"
-sudo scutil --set HostName "homer"
-sudo scutil --set LocalHostName "homer"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "homer"
-
-# Set fast key repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 2
-defaults write NSGlobalDomain InitialKeyRepeat -int 15
-
-
-# Require password as soon as screensaver or sleep mode starts
-# defaults write com.apple.screensaver askForPassword -int 1
-# defaults write com.apple.screensaver askForPasswordDelay -int 0
-
-# Disable the sound effects on boot
-sudo nvram SystemAudioVolume=" "
-
-# Expand save panel by default
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
-
-# Expand print panel by default
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
-
-# Save to disk (not to iCloud) by default
-defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
-
-# Automatically quit printer app once the print jobs complete
-defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
-
-# Disable the “Are you sure you want to open this application?” dialog
-defaults write com.apple.LaunchServices LSQuarantine -bool false
-
-# Show filename extensions by default
-defaults write NSGlobalDomain AppleShowAllExtensions -bool true
-
-# Disable transparency in the menu bar and elsewhere on Yosemite
-defaults write com.apple.universalaccess reduceTransparency -bool true
-
-# Disable Notification Center and remove the menu bar icon
-launchctl unload -w /System/Library/LaunchAgents/com.apple.notificationcenterui.plist 2> /dev/null
-
-# Disable smart quotes as they’re annoying when typing code
-defaults write NSGlobalDomain NSAutomaticQuoteSubstitutionEnabled -bool false
-
-# Disable smart dashes as they’re annoying when typing code
-defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
-
-# Disable “natural” (Lion-style) scrolling
-defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
-
-# Increase sound quality for Bluetooth headphones/headsets
-defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
-
-# Enable full keyboard access for all controls
-# (e.g. enable Tab in modal dialogs)
-defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
-
-# Trackpad: map bottom right corner to right-click
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadCornerSecondaryClick -int 2
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadRightClick -bool true
-defaults -currentHost write NSGlobalDomain com.apple.trackpad.trackpadCornerClickBehavior -int 1
-defaults -currentHost write NSGlobalDomain com.apple.trackpad.enableSecondaryClick -bool true
-
-# Disable press-and-hold for keys in favor of key repeat
-defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-
-# Set the timezone; see `sudo systemsetup -listtimezones` for other values
-sudo systemsetup -settimezone "America/New_York" > /dev/null
-
-# Stop iTunes from responding to the keyboard media keys
-launchctl unload -w /System/Library/LaunchAgents/com.apple.rcd.plist 2> /dev/null
-
-# Save screenshots to the desktop
-defaults write com.apple.screencapture location -string "${HOME}/Desktop"
-
-# Set Desktop as the default location for new Finder windows
-# For other paths, use `PfLo` and `file:///full/path/here/`
-defaults write com.apple.finder NewWindowTarget -string "PfDe"
-defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/Desktop/"
-
-# Display full POSIX path as Finder window title
-defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
-
-# Disable the warning when changing a file extension
-defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
-
-# Avoid creating .DS_Store files on network or USB volumes
-defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
-defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
-
-# Automatically open a new Finder window when a volume is mounted
-defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
-defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
-defaults write com.apple.finder OpenWindowForNewRemovableDisk -bool true
-
-# Show the ~/Library folder
-chflags nohidden ~/Library
-
-# Show the /Volumes folder
-sudo chflags nohidden /Volumes
-
-# Use list view in all Finder windows by default
-# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
-defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
-
-# Enable Safari’s debug menu
-defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
-
-# Prevent Time Machine from prompting to use new hard drives as backup volume
-defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
-
-# Enable the automatic update check
-defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
-
-# Check for software updates daily, not just once per week
-defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
-
-# Download newly available updates in background
-defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
-
-# Install System data files & security updates
-defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1
-
-# Turn on app auto-update
-defaults write com.apple.commerce AutoUpdate -bool true
-
-# Prevent Photos from opening automatically when devices are plugged in
-defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
-
-# Start SizeUp at login
-defaults write com.irradiatedsoftware.SizeUp StartAtLogin -bool true
-
-# Don’t show the preferences window on next start
-defaults write com.irradiatedsoftware.SizeUp ShowPrefsOnNextStart -bool false
-
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Check for Homebrew,
-# Install if we don't have it
-if test ! $(which brew); then
-  echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# homebrew
+export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+if hash brew &>/dev/null; then
+	echo_ok "Homebrew already installed. Getting updates..."
+	brew update
+	brew doctor
+else
+	echo_warn "Installing homebrew..."
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 # Update homebrew recipes
 brew update
 
 # Install GNU core utilities (those that come with OS X are outdated)
-brew tap homebrew/dupes
-brew install coreutils
-brew install gnu-sed --with-default-names
-brew install gnu-tar --with-default-names
-brew install gnu-indent --with-default-names
-brew install gnu-which --with-default-names
-brew install gnu-grep --with-default-names
-
+# brew tap homebrew/dupes
+# brew install coreutils
+# brew install gnu-sed --with-default-names
+# brew install gnu-tar --with-default-names
+# brew install gnu-indent --with-default-names
+# brew install gnu-which --with-default-names
+# brew install gnu-grep --with-default-names
 
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
-brew install findutils
+# brew install findutils
 
 # Install Bash 4
 brew install bash
 
+PACKAGES=(
+	cask
+	git
+	node
+  ripgrep
+	wget
+	zsh
+)
 
-echo "Installing packages..."
-brew install ${PACKAGES[@]}
+echo_ok "Installing packages..."
+brew install "${PACKAGES[@]}"
 
-echo "Cleaning up..."
+echo_ok "Cleaning up..."
 brew cleanup
 
-echo "Installing cask..."
-brew install caskroom/cask/brew-cask
-brew tap caskroom/versions
+echo_ok "Installing cask..."
+# brew install caskroom/cask/brew-cask
+brew tap caskroom/cask
 
-echo "Installing cask apps..."
-brew cask install ${CASKS[@]}
+CASKS=(
+	daisydisk
+	dropbox
+	firefox
+	google-chrome
+	iterm2
+	keybase
+  react-native-debugger
+	slack
+	spotify
+	spotify-notifications
+	virtualbox
+	visual-studio-code
+	vlc
+)
 
-echo "Set up NVM"
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
-source $(brew --prefix nvm)/nvm.sh
-nvm install 4.2.3
-nvm use 4.2.3
-nvm alias default node
+echo_ok "Installing cask apps..."
+brew cask install "${CASKS[@]}"
 
-# add paths to .profile (if not already present)
-grep -q "prompt.sh" ~/.profile || echo "source $DIR/prompt.sh" >> ~/.profile
+# brew cask quicklook
+echo_ok "Installing QuickLook Plugins..."
+brew cask install \
+	qlcolorcode qlmarkdown qlprettypatch qlstephen \
+	qlimagesize \
+	quicklook-csv quicklook-json epubquicklook
 
-for app in "Activity Monitor" \
-	"Address Book" \
-	"Calendar" \
-	"cfprefsd" \
-	"Contacts" \
-	"Dock" \
-	"Finder" \
-	"Google Chrome" \
-	"Mail" \
-	"Messages" \
-	"Photos" \
-	"Safari" \
-	"SizeUp" \
-	"SystemUIServer" \
-	"Terminal" \
-	"iCal"; do
-	killall "${app}" &> /dev/null
-done
-echo "Done. Note that some of these changes require a logout/restart to take effect."
+echo_ok "Installing fonts..."
+brew tap caskroom/fonts
+FONTS=(
+	font-clear-sans
+	font-consolas-for-powerline
+	font-dejavu-sans-mono-for-powerline
+	font-fira-code
+	font-fira-mono-for-powerline
+	font-inconsolata
+	font-inconsolata-for-powerline
+  font-iosevka
+	font-liberation-mono-for-powerline
+	font-menlo-for-powerline
+	font-roboto
+)
+brew cask install "${FONTS[@]}"
 
-echo "Bootstrapping complete"
+echo_ok "Installing global npm packages..."
+
+npm install -g ts-node
+npm install -g typescript
+npm install -g yarn
+
+echo_ok "Installing oh my zsh..."
+
+if [[ ! -f ~/.zshrc ]]; then
+	echo ''
+	echo '##### Installing oh-my-zsh...'
+	curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+	cp ~/.zshrc ~/.zshrc.orig
+	cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+	chsh -s /bin/zsh
+fi
+
+echo_ok "Configuring Github"
+
+if [[ ! -f ~/.ssh/id_rsa ]]; then
+	echo ''
+	echo '##### Please enter your github username: '
+	read github_user
+	echo '##### Please enter your github email address: '
+	read github_email
+
+	# setup github
+	if [[ $github_user && $github_email ]]; then
+		# setup config
+		git config --global user.name "$github_user"
+		git config --global user.email "$github_email"
+		git config --global github.user "$github_user"
+		# git config --global github.token your_token_here
+		git config --global color.ui true
+		git config --global push.default current
+		# VS Code support
+		git config --global core.editor "code --wait"
+
+		# set rsa key
+		curl -s -O http://github-media-downloads.s3.amazonaws.com/osx/git-credential-osxkeychain
+		chmod u+x git-credential-osxkeychain
+		sudo mv git-credential-osxkeychain "$(dirname $(which git))/git-credential-osxkeychain"
+		git config --global credential.helper osxkeychain
+
+		# generate ssh key
+		cd ~/.ssh || exit
+		ssh-keygen -t rsa -C "$github_email"
+		pbcopy <~/.ssh/id_rsa.pub
+		echo ''
+		echo '##### The following rsa key has been copied to your clipboard: '
+		cat ~/.ssh/id_rsa.pub
+		echo '##### Follow step 4 to complete: https://help.github.com/articles/generating-ssh-keys'
+		ssh -T git@github.com
+	fi
+fi
+
+echo_ok "Installing VS Code Extensions..."
+
+VSCODE_EXTENSIONS=(
+	shan.code-settings-sync
+)
+
+if hash code &>/dev/null; then
+	echo_ok "Installing VS Code extensions..."
+	for i in "${VSCODE_EXTENSIONS[@]}"; do
+		code --install-extension "$i"
+	done
+fi
+
+echo_ok "Configuring OSX..."
+
+# Set fast key repeat rate
+# The step values that correspond to the sliders on the GUI are as follow (lower equals faster):
+# KeyRepeat: 120, 90, 60, 30, 12, 6, 2
+# InitialKeyRepeat: 120, 94, 68, 35, 25, 15
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
+
+# Show filename extensions by default
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+# Expanded Save menu
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+# Expanded Print menu
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+# Disable "natural" scroll
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+echo_ok 'Running OSX Software Updates...'
+sudo softwareupdate -i -a
+
+echo_ok "Creating folder structure..."
+[[ ! -d Projects ]] && mkdir Projects
+#[[ ! -d Workspace ]] && mkdir Workspace
+
+echo_ok "Bootstrapping complete"
